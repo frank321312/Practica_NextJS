b421262b91cddd0fc0a9b07471f43930b842e319
Zein2006 <davidbzk95@gmail.com>
2024-05-10 01:37:59 -0300

    Cuarto commit

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -6,13 +6,19 @@ interface User {
 }
 
 export async function GET(request: any, { params }: { params: User }) {
+    // Obtenemos un usuario especifico que pide el cliente
     const res = await axios.get(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
-    const { searchParams } = new URL(request.url); 
-    const nombre = searchParams.get("name");
-    
+
+    // Creamos un objeto URL que obtendra la url completa, luego la desestructuramos a partir del signo de pregunta ("?")
+    const { searchParams } = new URL(request.url);  // Ejemplo { "nombre": "Pepe" }
+
+    // Ahora obtenemos el valor de la propiedad name del objeto searchParams a traves del metodo get()
+    const nombre = searchParams.get("name"); // Esto es igual a "Pepe"
+    console.log(request.url);
     console.log(searchParams);
     console.log(nombre);
     // console.log(request);
 
+    // Usamos NextResponse para responder al cliente en un formato json
     return NextResponse.json(res.data);
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,14 +4,18 @@ import axios from "axios";
 console.log(process.env.SECRET_TOKEN);
 
 export async function GET() {
+    // En la ruta http://localhost:3000/api/users devolvera todos los usuarios deuuelto por jsonplaceholder
     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     return NextResponse.json(res.data);
 }
 
 export async function POST(request: any) {
+    // req obtiene los datos enviados al servidor, si no se envia nada lanzara un error, se recomienda usar postman
+    // req es una promesa por lo que podemos usar async/await para obtener su valor. Otra forma es usando then
     const req = await request.json();
     console.log(req);
 
+    // NextResponse es una clase que proporciona next, permite personalizar la respuesta que se envia del servidor al cliente.
     return NextResponse.json({ message: "Creando datos..." });
 }
 
